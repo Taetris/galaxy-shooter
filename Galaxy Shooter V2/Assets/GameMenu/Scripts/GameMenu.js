6b342b8721a3f570382c78bd0abd8543386a2ecc
Ahmed Arnaut <ahmed.arnaut@hotmail.com>
2017-12-27 15:26:48 +0100

    Added GameMenu Scene

---
+++ Galaxy Shooter V2/Assets/GameMenu/Scripts/GameMenu.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public class GameMenu extends MonoBehaviour { 
+
+	function onPlayButtonClicked() {
+		SceneManagement.SceneManager.LoadScene("PlayScene");
+	}
+
+	function onScoreboardButtonClicked() {
+		SceneManagement.SceneManager.LoadScene("ScoreboardScene");
+	}
+
+	function onExitButtonClicked() {
+		Application.Quit();
+	}
+}
+
